6414817717a7e068eb635916168420295c257d4c
nicodeheza <nicolasdeheza@hotmail.com>
2023-02-22 18:00:17 -0300

    godotenv

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
     app := fiber.New()
 	app.Use(logger.New())
 
