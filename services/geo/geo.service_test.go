d9599511310f152009ee1d2f6e129a435d7f7c2d
nicodeheza <nicolasdeheza@hotmail.com>
2023-03-10 13:34:54 -0300

    goe service

---
+++ services/geo/geo.service_test.go
@@ -0,0 +1,190 @@
+package geo
+
+import (
+	"math"
+	"testing"
+
+	"github.com/nicodeheza/peersEat/models"
+)
+
+type GetCoorDistanceTests struct {
+	Coor1 models.Center
+	Coor2 models.Center
+	result float64
+
+}
+
+func TestGetCoorDistance(t *testing.T) {
+
+	tests:= []GetCoorDistanceTests{
+		{
+			models.Center{Long: -36.0233352, Lat: -61.2301026}, 
+			models.Center{Long: -36.1414237, Lat: -59.7854004}, 
+			160.765761,
+		},
+		{
+			models.Center{Long: 54.4828045, Lat: -312.1237794}, 
+			models.Center{Long: 54.4904641, Lat: -311.7656207}, 
+			39.827583,
+		},
+		{
+			models.Center{Long: 54.8380309, Lat: -476.8637697}, 
+			models.Center{Long: 54.8194413, Lat: -476.8588257}, 
+			1.083700,
+		},
+	}
+
+	for _, test:= range tests{
+		result :=GetCoorDistance(test.Coor1, test.Coor2)
+		dif := math.Abs(result - test.result)
+		if dif > 0.001{
+			t.Errorf("expect %f but gets %f for %f, %f - %f, %f", 
+			test.result, result, test.Coor1.Lat, test.Coor1.Long, 
+			test.Coor2.Lat, test.Coor2.Long)
+		}
+	}
+}
+
+type IsSameCoorTest struct{
+	Coor1 models.Center
+	Coor2 models.Center
+	result bool
+}
+
+func TestIsSameCoor(t *testing.T){
+	tests := []IsSameCoorTest{
+			{
+				models.Center{Long: -36.0233352, Lat: -61.2301026}, 
+				models.Center{Long: -36.0233352, Lat: -59.7854004}, 
+				false,
+			},
+			{
+				models.Center{Long: 54.4828045, Lat: -312.1237794}, 
+				models.Center{Long: 54.4828045, Lat: -312.1237794}, 
+				true,
+			},
+			
+		}
+	for _, test := range tests{
+
+		result := IsSameCoor(test.Coor1, test.Coor2)
+
+		if result != test.result{
+			t.Errorf("expect %t, but gets %t on (%v, %v)", 
+			test.result, result, test.Coor1, test.Coor2)
+		}
+	}
+	
+}
+
+type IsInInfluenceAreaTest struct{
+	SelfPeer models.Peer
+	Peer models.Peer
+	result bool
+}
+
+func TestIsInInfluenceArea(t *testing.T){
+
+	basePeer := models.Peer{
+		Url: "http://test.com",
+		Center: models.Center{Long: -36.0233352, Lat: -61.2301026},
+		City: "test city",
+		Country: "test country",
+	}
+	peer1 := basePeer
+	peer1.Center = models.Center{Long: -32.0233352, Lat: -61.2301026}
+
+	peer2 := basePeer
+	peer2.InfluenceRadius= 1
+	peer2.DeliveryRadius=2
+	peer2.Center= models.Center{Long: -34.605447, Lat: -58.383594}
+
+	peer3 := peer2
+	peer3.Center= models.Center{Long: -34.605341, Lat: -58.386598}
+
+	peer4 := peer2
+	peer4.Center= models.Center{Long: -34.606716, Lat: -58.470303}
+	tests := []IsInInfluenceAreaTest{
+		{
+		 basePeer, 
+		 basePeer, 
+		 true,
+		},
+		{
+		 basePeer, 
+		 peer1, 
+		 false,
+		},
+		{
+		 peer2, 
+		 peer3, 
+		 true,
+		},
+		{
+		 peer2, 
+		 peer4, 
+		 false,
+		},
+	}
+
+	for _, test:= range tests{
+		result := IsInInfluenceArea(test.SelfPeer, test.Peer)
+
+		if result != test.result{
+			t.Errorf("expect %t but gets %t for (%v, %v)", test.result, result, test.SelfPeer, test.Peer)
+		}
+	}
+}
+
+func TestIsInDeliveryArea(t *testing.T){
+
+	basePeer := models.Peer{
+		Url: "http://test.com",
+		Center: models.Center{Long: -36.0233352, Lat: -61.2301026},
+		City: "test city",
+		Country: "test country",
+	}
+	peer1 := basePeer
+	peer1.Center = models.Center{Long: -32.0233352, Lat: -61.2301026}
+
+	peer2 := basePeer
+	peer2.InfluenceRadius= 1
+	peer2.DeliveryRadius=2
+	peer2.Center= models.Center{Long: -34.605447, Lat: -58.383594}
+
+	peer3 := peer2
+	peer3.Center= models.Center{Long: -34.605341, Lat: -58.386598}
+
+	peer4 := peer2
+	peer4.Center= models.Center{Long: -34.606716, Lat: -58.470303}
+	tests := []IsInInfluenceAreaTest{
+		{
+		 basePeer, 
+		 basePeer, 
+		 true,
+		},
+		{
+		 basePeer, 
+		 peer1, 
+		 false,
+		},
+		{
+		 peer2, 
+		 peer3, 
+		 true,
+		},
+		{
+		 peer2, 
+		 peer4, 
+		 false,
+		},
+	}
+
+	for _, test:= range tests{
+		result := IsInDeliveryArea(test.SelfPeer, test.Peer)
+
+		if result != test.result{
+			t.Errorf("expect %t but gets %t for (%v, %v)", test.result, result, test.SelfPeer, test.Peer)
+		}
+	}
+}
