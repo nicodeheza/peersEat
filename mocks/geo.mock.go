5ad6eb2ec70546e15f7ae6b8f296ea3ebd5cbe38
nicodeheza <nicolasdeheza@hotmail.com>
2023-03-13 18:41:49 -0300

    goe mock

---
+++ mocks/geo.mock.go
@@ -0,0 +1,39 @@
+package mocks
+
+import (
+	"github.com/nicodeheza/peersEat/models"
+)
+
+type GeoService struct{}
+
+func NewGeo() *GeoService{
+	return &GeoService{}
+}
+
+func (g GeoService) GetCoorDistance(coor1 models.Center, coor2 models.Center) float64{
+	return coor1.Long
+}
+
+func (g GeoService) IsSameCoor( coor1 models.Center, coor2 models.Center) bool {
+	if coor1.Lat == coor2.Lat &&
+	 coor1.Long == coor2.Long{
+		return true
+	}
+
+	return false
+}
+
+func (g GeoService) IsInInfluenceArea(selfPeer models.Peer, peer models.Peer) bool{
+	if selfPeer.Url == "true"{
+		return true
+	}
+	return false
+}
+
+func (g GeoService) IsInDeliveryArea(selfPeer models.Peer, peer models.Peer) bool{
+	if peer.Url == "true"{
+		return true
+	}
+	return false
+}
+
