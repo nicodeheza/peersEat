7b759dc2f760ea0bc81e89d5cdd1d27720f037b7
nicodeheza <nicolasdeheza@hotmail.com>
2023-06-03 17:54:08 -0300

    refactor

--- modules/index.go
+++ modules/index.go
@@ -2,16 +2,38 @@ package modules
 
 import (
 	"github.com/go-playground/validator/v10"
+	"github.com/nicodeheza/peersEat/controllers"
+	"github.com/nicodeheza/peersEat/events"
 	"github.com/nicodeheza/peersEat/middleware"
+	"github.com/nicodeheza/peersEat/models"
+	"github.com/nicodeheza/peersEat/repositories"
+	"github.com/nicodeheza/peersEat/services"
 	"github.com/nicodeheza/peersEat/services/geo"
 	"github.com/nicodeheza/peersEat/services/validations"
 	"github.com/nicodeheza/peersEat/utils"
 )
 
-type Application struct {
-	Peer           *PeerModule
-	Restaurant     *RestaurantModule
-	AuthMiddleware *middleware.AuthMiddleware
+func initRepositories() *Repositories {
+	restaurantCollection := models.GetRestaurantColl("peersEatDB")
+	restaurantRepository := repositories.NewRestaurantRepository(restaurantCollection)
+
+	peerCollection := models.GetPeerColl("peersEatDB")
+	peerRepository := repositories.NewPeerRepository(peerCollection)
+
+	return &Repositories{Peer: peerRepository, Restaurant: restaurantRepository}
+}
+
+func initServices(repos *Repositories, authHelpers *utils.AuthHelpers, eventLoop *events.EventLoop, geo *geo.GeoService) *Services {
+	restaurant := services.NewRestaurantService(repos.Restaurant, authHelpers, geo)
+	peer := services.NewPeerService(repos.Peer, geo, repos.Restaurant, eventLoop)
+
+	return &Services{peer, restaurant}
+}
+
+func initControllers(services *Services, validate *validations.Validate, geo *geo.GeoService) *Controllers {
+	restaurant := controllers.NewRestaurantController(services.restaurant, services.peer, validate)
+	peer := controllers.NewPeerController(services.peer, validate, services.restaurant, geo)
+	return &Controllers{peer, restaurant}
 }
 
 func InitApp() *Application {
@@ -19,8 +41,15 @@ func InitApp() *Application {
 	geo := geo.NewGeo()
 	validate := validations.NewValidator(validator.New())
 	authHelpers := utils.NewAuthHelper()
-	restaurantModule := NewRestaurantModule(authHelpers, geo)
-	peerModule := newPeerModule(validate, geo, restaurantModule.Service, restaurantModule.Repository)
+
+	repos := initRepositories()
+	eventHandlers := events.NewEventHandlers(repos.Peer, validate, geo)
+	eventLoop := events.InitEventLoop(eventHandlers)
+	services := initServices(repos, authHelpers, eventLoop, geo)
+	controllers := initControllers(services, validate, geo)
+
+	restaurantModule := &RestaurantModule{repos.Restaurant, services.restaurant, controllers.restaurant}
+	peerModule := &PeerModule{repos.Peer, services.peer, controllers.peer}
 	authMiddleware := middleware.InitAuthMiddleware(restaurantModule.Service)
 
 	return &Application{peerModule, restaurantModule, authMiddleware}
